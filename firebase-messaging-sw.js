3bc29429c42d6db6e4d1e23af0b3d0d965854d9c
muklasar <muklasar@gmail.com>
2021-03-03 17:50:30 +0530

    assdsdaaaaaaaaaa1aaaaaaaaa

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
+
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDLjiNnsvXnYIx_oKm4k0_VEyCgk7Tww_o",
-    authDomain: "auth-29.firebaseapp.com",
-    projectId: "auth-29",
-    storageBucket: "auth-29.appspot.com",
-    messagingSenderId: "718208349726",
-    appId: "1:718208349726:web:2ba4a1adb09e6dd0ab200f",
-    measurementId: "G-9D1H9HZH4Y"
+  apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
+  authDomain: "swagit-authentication.firebaseapp.com",
+  databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
+  projectId: "swagit-authentication",
+  storageBucket: "swagit-authentication.appspot.com",
+  messagingSenderId: "756142162673",
+  appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
+  measurementId: "G-R2BLQHPN4N"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
