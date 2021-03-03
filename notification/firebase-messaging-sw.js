87dc78fd2af51d97524b32ee4fcaa9674021368c
muklasar <muklasar@gmail.com>
2021-03-03 17:55:09 +0530

    assdsdaaaaaaaaaa1aaaaaaaaaaaa

--- notification/firebase-messaging-sw.js
+++ notification/firebase-messaging-sw.js
@@ -1,19 +1,19 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
-  authDomain: "swagit-authentication.firebaseapp.com",
-  databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
-  projectId: "swagit-authentication",
-  storageBucket: "swagit-authentication.appspot.com",
-  messagingSenderId: "756142162673",
-  appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
-  measurementId: "G-R2BLQHPN4N"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
+//   authDomain: "swagit-authentication.firebaseapp.com",
+//   databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
+//   projectId: "swagit-authentication",
+//   storageBucket: "swagit-authentication.appspot.com",
+//   messagingSenderId: "756142162673",
+//   appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
+//   measurementId: "G-R2BLQHPN4N"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 
 // const messaging = firebase.messaging();
