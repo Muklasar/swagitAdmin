5775c2649a6ce91bd881e2c2bda1034fbe9f1ef3
muklasar <muklasar@gmail.com>
2021-03-03 17:59:31 +0530

    assdsdaaaaaaaaaa1aaaaaaaaaaaaaa

--- notification/firebase-messaging-sw.js
+++ notification/firebase-messaging-sw.js
@@ -1,45 +1,45 @@
-// importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js');
 
 
-// var firebaseConfig = {
-//   apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
-//   authDomain: "swagit-authentication.firebaseapp.com",
-//   databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
-//   projectId: "swagit-authentication",
-//   storageBucket: "swagit-authentication.appspot.com",
-//   messagingSenderId: "756142162673",
-//   appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
-//   measurementId: "G-R2BLQHPN4N"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+  apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
+  authDomain: "swagit-authentication.firebaseapp.com",
+  databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
+  projectId: "swagit-authentication",
+  storageBucket: "swagit-authentication.appspot.com",
+  messagingSenderId: "756142162673",
+  appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
+  measurementId: "G-R2BLQHPN4N"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage(function(payload) {
-//   console.log('Received background message ', payload);
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
 
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//   };
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
 
-//   self.registration.showNotification(notificationTitle,
-//     notificationOptions);
-// });
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
 
-// messaging.getToken({ vapidKey: 'BApI0asOcygMhR0DdrE-6-D15PHOQOtHOvZKP1RxedHu3bqDTyuyIE3lXb-1LkJoOqDP2Q9kfWWT9TKzfgz61Ak' }).then((currentToken) => {
-//   if (currentToken) {
-//       console.log(currentToken)
-//       // ...
-//   } else {
-//       // Show permission request UI
-//       console.log('No registration token available. Request permission to generate one.');
-//       // ...
-//   }
-// }).catch((err) => {
-//   console.log('An error occurred while retrieving token. ', err);
-//   // ...
-// });
+messaging.getToken({ vapidKey: 'BApI0asOcygMhR0DdrE-6-D15PHOQOtHOvZKP1RxedHu3bqDTyuyIE3lXb-1LkJoOqDP2Q9kfWWT9TKzfgz61Ak' }).then((currentToken) => {
+  if (currentToken) {
+      console.log(currentToken)
+      // ...
+  } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
