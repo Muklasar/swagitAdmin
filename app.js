cb862cca46e3a7b733249d5673844c89dc99f6ad
muklasar <muklasar@gmail.com>
2021-03-03 15:43:50 +0530

    as

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js")
-importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js")
 
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyDLjiNnsvXnYIx_oKm4k0_VEyCgk7Tww_o",
-    authDomain: "auth-29.firebaseapp.com",
-    projectId: "auth-29",
-    storageBucket: "auth-29.appspot.com",
-    messagingSenderId: "718208349726",
-    appId: "1:718208349726:web:2ba4a1adb09e6dd0ab200f",
-    measurementId: "G-9D1H9HZH4Y"
+    apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
+    authDomain: "swagit-authentication.firebaseapp.com",
+    databaseURL: "https://swagit-authentication-default-rtdb.firebaseio.com",
+    projectId: "swagit-authentication",
+    storageBucket: "swagit-authentication.appspot.com",
+    messagingSenderId: "756142162673",
+    appId: "1:756142162673:web:133a71fdd7f5c2a0b61f50",
+    measurementId: "G-R2BLQHPN4N"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-
-const messaging = firebse.messaging();
+const messaging = firebase.messaging();
 messaging.requestPermission()
     .then(function () {
         console.log('Have permission')
+        return messaging.getToken();
     })
     .then(function (token) {
         console.log(token)
@@ -27,3 +28,22 @@ messaging.requestPermission()
     .catch(function (err) {
         console.log('error Occured')
     })
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({ vapidKey: '756142162673' }).then((currentToken) => {
+    if (currentToken) {
+        console.log(currentToken)
+        // Send the token to your server and update the UI if necessary
+        // ...
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
+
+
