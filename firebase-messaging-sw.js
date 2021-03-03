618145761d4020fe0e946f2dcfa40cfe78030f88
muklasar <muklasar@gmail.com>
2021-03-03 16:34:52 +0530

    assdsdaaaaa

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js")
+importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js");
 
 var firebaseConfig = {
     apiKey: "AIzaSyDOralpZ69Eh_YMOHWR8LHRPtKZTBQFtMs",
@@ -17,14 +17,18 @@ firebase.initializeApp(firebaseConfig);
 
 
 
-const messaging = firebse.messaging();
-messaging.requestPermission()
-    .then(function () {
-        console.log('Have permission')
-    })
-    .then(function (token) {
-        console.log(token)
-    })
-    .catch(function (err) {
-        console.log('error Occured')
-    })
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken({ vapidKey: 'BApI0asOcygMhR0DdrE-6-D15PHOQOtHOvZKP1RxedHu3bqDTyuyIE3lXb-1LkJoOqDP2Q9kfWWT9TKzfgz61Ak' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
