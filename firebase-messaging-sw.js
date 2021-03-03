2e9c936ce75d760b02378fcb3967b6163aeede79
muklasar <muklasar@gmail.com>
2021-03-03 17:40:57 +0530

    assdsdaaaaaaaaaa1aaaaaaa

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScript("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js");
-importScript("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.9/firebase-messaging.js");
 
 
 var firebaseConfig = {
